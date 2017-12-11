9b56aead6055556a2e4514669b775bcd03b8302c
EdgarMagdaleno <edgarmv97@gmail.com>
2017-12-11 13:57:47 -0800

    u

--- sketch.js
+++ sketch.js
@@ -1,25 +1,25 @@
 var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Composites = Matter.Composites,
-    Common = Matter.Common,
-    Constraint = Matter.Constraint,
-    MouseConstraint = Matter.MouseConstraint,
-    Mouse = Matter.Mouse,
-    World = Matter.World,
-    Bodies = Matter.Bodies;
+	Render = Matter.Render,
+	Runner = Matter.Runner,
+	Composites = Matter.Composites,
+	Common = Matter.Common,
+	Constraint = Matter,
+	MouseConstraint = Matter.MouseConstraint,
+	Mouse = Matter.Mouse,
+	World = Matter.World,
+	Bodies = Matter.Bodies;
 
 var engine = Engine.create(),
-    world = engine.world;
+	world = engine.world;
 
 var render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        width: 2000,
-        height: 600,
-        showAngleIndicator: true
-    }
+	element: document.body,
+	engine: engine,
+	options: {
+		width: 2000,
+		height: 600,
+		showAngleIndicator: true
+	}
 });
 
 Render.run(render);
@@ -28,92 +28,101 @@ var runner = Runner.create();
 Runner.run(runner, engine);
 
 World.add(world, [
-    Bodies.rectangle(400, 0, 3000, 50, { isStatic: true }),
-    Bodies.rectangle(400, 600, 3000, 50, { isStatic: true }),
-    Bodies.rectangle(1900, 300, 50, 600, { isStatic: true }),
-    Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
+	Bodies.rectangle(400, 0, 3000, 50, { isStatic: true }),
+	Bodies.rectangle(400, 600, 3000, 50, { isStatic: true }),
+	Bodies.rectangle(1900, 300, 50, 600, { isStatic: true }),
+	Bodies.rectangle(0, 300, 50, 600, { isStatic: true })
 ]);
 
 var mouse = Mouse.create(render.canvas),
-    mouseConstraint = MouseConstraint.create(engine, {
-        mouse: mouse,
-        constraint: {
-            angularStiffness: 0,
-            render: {
-                visible: false
-            }
-        }
-    });
+	mouseConstraint = MouseConstraint.create(engine, {
+		mouse: mouse,
+		constraint: {
+			angularStiffness: 0,
+			render: {
+				visible: false
+			}
+		}
+	});
 
 World.add(world, mouseConstraint);
 
 render.mouse = mouse;
 
 Render.lookAt(render, {
-    min: { x: 0, y: 0 },
-    max: { x: 2000, y: 600 }
+	min: { x: 0, y: 0 },
+	max: { x: 2000, y: 600 }
 });
 
+rnd = function(max, min) {
+	return Math.floor((Math.random() * (max - min)) + min);
+}
+
+contract = function(muscle) {
+	console.log("Contract");
+	let tmp = muscle.length;
+	muscle.length = muscle.extended_length;
+	muscle.extended_length = tmp;
+
+	setTimeout(contract, 500);
+}
+
+var Creature = function() {
+	this.nodes = [];
+	this.muscles = [];
+	var self = this;
+
+	this.build = function() {
+	   this.nodes.length = rnd(3, 6);
+
+		for(let i = 0; i < this.nodes.length; i++) {
+			this.nodes[i] = Bodies.circle(rnd(300, 140), rnd(300, 140), 20);
+		}
+
+		for(let i = 0; i < this.nodes.length - 1; i++) {
+			this.muscles[i] =  Matter.Constraint.create({
+				pointA: {
+					x : 0,
+					y : 0
+				},
+				pointB: {
+					x : 0,
+					y : 0 
+				},
+				bodyA: this.nodes[i],
+				bodyB: this.nodes[i + 1],
+				stiffness: 0.05,
+				length: rnd(100, 70),
+				extended_length : rnd(length, length + 50)
+			});
+		}
+	}
+
+	this.contract = function() {
+		for(let i = 0; i < self.muscles.length; i++) {
+			let tmp = self.muscles[i].length;
+			self.muscles[i].length = self.muscles[i].extended_length;
+			self.muscles[i].extended_length = tmp;
+		}
+	}
+
+	this.spawn = function() {
+		for(let i = 0; i < this.nodes.length; i++) {
+			World.add(world, this.nodes[i]);
+		}
+
+		for(let i = 0; i < this.muscles.length; i++) {
+			World.add(world, this.muscles[i]);
+		}
+	}
+
+	this.interval = setInterval(this.contract, 200);
+}
+
 start = function() {
-    setTimeout(start, 1000);
+	let c = new Creature();
+	c.build();
+	c.spawn();
 }
 
-rnd = function(max, min, not, also_not) {
-    let output;
-    do {
-        output = Math.floor((Math.random() * (max - min)) + min);
-    } while(output === not || output === also_not);
-    return output;
-}
-
-var constraints = [];
-var extensions = [];
-
-create_body = function() {
-    var bodies = [];
-    var n = rnd(6, 3);
-    console.log("Bodies: " + n);
-    for(i = 0; i < n; i++) {
-        bodies[i] = Bodies.circle(rnd(300, 140), rnd(300, 140), 20);
-    }
-
-    for(i = 0; i < n; i++) {
-        var index2 = rnd(n - 1, 0, i, bodies[i].connection);
-        bodies[i].connection = index2;
-        constraints[i] = Matter.Constraint.create({
-            pointA: {x:0, y:0},
-            pointB: {x:0, y:0},
-            bodyA: bodies[i],
-            bodyB: bodies[index2],
-            stiffness: 0.05,
-            length: rnd(140, 50)
-        });
-
-        extensions[i] = rnd(constraints[i].length + 150, constraints[i].length);
-        console.log(extensions[i]);
-    }
-
-    for(i = 0; i < n; i++) {
-        World.add(world, bodies[i]);
-    }
-
-    for(i = 0; i < n; i++) {
-        World.add(world, constraints[i]);
-    }
-}
-
-start = function() {
-    for(i = 0; i < constraints.length; i++) {
-        console.log("Before: " + constraints[i].length);
-        let tmp = extensions[i];
-        extensions[i] = constraints[i].length;
-        constraints[i].length = tmp;
-
-        console.log("After: " + constraints[i].length);
-    }
-
-    setTimeout(start, 500);
-}
-
-create_body();
 start();
